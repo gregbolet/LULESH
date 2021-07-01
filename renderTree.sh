0b7974fb08b63932d6c8078883920cd1f49521c4
gregboleet <gbolet@vt.edu>
2021-07-01 13:28:09 -0700

    got rid of old render tree file

--- renderTree.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-~/workspace/apollo/src/python/analysis/rtree2dot.py -i ~/workspace/lulesh/build/dtree-latest-rank-0-CalcElemVolumeDerivative--hourglass.yaml \
-	-o ~/workspace/lulesh/build/treefile.dot
-
-dot -Tpdf treefile.dot >> treefile.pdf
-
