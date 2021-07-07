bd86a9606a74d7df146a15a23f951b19c1776760
gregboleet <gbolet@vt.edu>
2021-07-07 11:33:38 -0700

    backup

---
+++ analyzeTraces.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+EXEC_DIR=~/workspace/lulesh/build
+TRACE_DIR=$EXEC_DIR/trace
+ANALY_DIR=~/workspace/apollo/src/python/analysis
+
+POLICIES=(0 1)
+
+# Do the trace analysis
+$ANALY_DIR/./analyze-traces.py --dir $TRACE_DIR --rr --random --nstatic ${#POLICIES[@]} --nranks 1 --opt
