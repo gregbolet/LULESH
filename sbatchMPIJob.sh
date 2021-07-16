17801ba60806553dcd35dddbe0fa259a5a7a0bd9
gregboleet <gbolet@vt.edu>
2021-07-16 09:04:49 -0700

    started MPI batch job script

---
+++ sbatchMPIJob.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=luleshMPITest
+#SBATCH --output=luleshMPIRun.log
+#SBATCH --ntasks=8
+#SBATCH --time=20:00
+
+export OMP_PLACES=cores
+export OMP_NUM_THREADS=36
+export OMP_WAIT_POLICY=active
+export OMP_PROC_BIND=close
+
+export APOLLO_COLLECTIVE_TRAINING=1
+export APOLLO_LOCAL_TRAINING=0
+export APOLLO_RETRAIN_ENABLE=0
+export APOLLO_SINGLE_MODEL=0
+export APOLLO_REGION_MODEL=1
+export APOLLO_STORE_MODELS=0
+export APOLLO_TRACE_MEASURES=0
+export APOLLO_TRACE_CSV=1
+export APOLLO_GLOBAL_TRAIN_PERIOD=10000
+export APOLLO_INIT_MODEL=Static,0
+
+# May want to try mpiexec instead -- we'll see
+srun ~/workspace/lulesh/build/lulesh2.0 -s 30 -i 1000 -r 100 -b 0 -c 0
