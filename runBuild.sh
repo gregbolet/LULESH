0ab27c694c491293107151ac2c8485d37bb2a03b
gregboleet <gbolet@vt.edu>
2021-07-16 12:53:29 -0700

    failed attempt as sbatch env vars

--- runBuild.sh
+++ runBuild.sh
@@ -13,7 +13,7 @@ export MPI_CXX=$(which mpicxx)
 
 cmake -DWITH_MPI=On -DWITH_OPENMP=On \
       -DCMAKE_BUILD_TYPE=Release \
-      -DWITH_APOLLO=Off \
+      -DWITH_APOLLO=On \
       -DAPOLLO_DIR=/g/g15/bolet1/workspace/apollo/ \
       -DWITH_CALIPER=Off \
       -DCALIPER_DIR=/usr/WS2/bolet1/spack/opt/spack/linux-rhel7-broadwell/gcc-10.2.1/caliper-2.5.0-bqvkflizcmjjpbjtwnatgwjpikgajjgd/ \
